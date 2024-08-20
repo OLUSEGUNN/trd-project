2cc670c10f0cf3a22998e033612373af8d97be33
OLUSEGUNN <159138132+OLUSEGUNN@users.noreply.github.com>
2024-08-20 05:36:31 -0700

    Add files via upload

---
+++ trd proj/trdproject.js
@@ -0,0 +1,12 @@
+
+const cancelMenu = document.getElementById('cancel-btn');
+
+
+var sectionIndex = 0;
+
+document.querySelector(".fa-bars").addEventListener("click", ()=>{
+    const responsiveNav = document.querySelector(".responsive-nav").style.display= "block"
+} );
+cancelMenu.addEventListener('click', () =>{
+    const responsiveNav = document.querySelector(".responsive-nav").style.display= "none" 
+});
